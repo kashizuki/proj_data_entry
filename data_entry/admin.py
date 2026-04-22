48ffb57e1216abb9019c9640737ab6cf9b1901e3
kashizuki <141428584+kashizuki@users.noreply.github.com>
2026-04-22 23:20:02 +0700

    Delete data_entry directory

--- data_entry/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Activity, ActivityPhoto, Project, UserProfile, Work
-
-
-admin.site.register(UserProfile)
-admin.site.register(Project)
-admin.site.register(Work)
-admin.site.register(Activity)
-admin.site.register(ActivityPhoto)
--- data_entry/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DataEntryConfig(AppConfig):
-    name = 'data_entry'
